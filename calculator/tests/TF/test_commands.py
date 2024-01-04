cab61b362d8a914a1cadd12047a63ced62e53ae3
Alexandre Mallet <alexandre.mallet@buldee.com>
2024-01-04 20:16:40 +0100

    test(error): Add one more covered usecase.

--- calculator/tests/TF/test_commands.py
+++ calculator/tests/TF/test_commands.py
@@ -1,10 +1,32 @@
 from django.test import TestCase
+from django.core.management import call_command
 from unittest.mock import patch, MagicMock, call
 from calculator.management.commands.freelance_calculate import Command
 from calculator.config import Messages
 
 
 class TestFreelanceCalculateCommand(TestCase):
+    @patch("calculator.management.commands.freelance_calculate.Command.get_input")
+    @patch("calculator.management.commands.freelance_calculate.Command.write_output")
+    def test_no_style_argument(self, mock_write_output, mock_get_input):
+        mock_get_input.side_effect = [
+            "500",
+            "5",
+            "non",
+        ]
+        call_command("freelance_calculate", no_style=True)
+
+    @patch("calculator.management.commands.freelance_calculate.Command.get_input")
+    @patch("calculator.management.commands.freelance_calculate.Command.write_output")
+    def test_default_style_behavior(self, mock_write_output, mock_get_input):
+        mock_get_input.side_effect = [
+            "500",
+            "5",
+            "oui",
+            "4",
+        ]
+        call_command("freelance_calculate")
+
     @patch(
         "calculator.management.commands.freelance_calculate.Command.get_input",
         side_effect=["500", "5", "oui", "4"],
