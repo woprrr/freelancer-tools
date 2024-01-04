ecbdd84f667bf7b8081627aa87b9b07589508a8d
Alexandre Mallet <alexandre.mallet@buldee.com>
2024-01-04 19:42:08 +0100

    fix(style): Fix env styling issue.

--- calculator/management/commands/freelance_calculate.py
+++ calculator/management/commands/freelance_calculate.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
-from enum import Enum
 from calculator.shared.business_logic import calculate_monthly_income, adjust_dar
 from calculator.config import Messages
+from enum import Enum
 
 
 class UserChoice(Enum):
@@ -18,36 +18,50 @@ PROMPT_NEW_WORK_DAYS = "📅 Entrez le nouveau nombre de jours de travail par se
 class Command(BaseCommand):
     help = "Calculateur de rémunération pour freelances"
 
-    def handle(self, *args, **kwargs):
-        self.stdout.write(Messages.REM_TITLE)
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--no-style", action="store_true", help="Diseable style output."
+        )
 
+    def handle(self, *args, **kwargs):
+        self.no_style = kwargs.get("no_style", False)
+
+        self.write_output(Messages.REM_TITLE)
         try:
-            dar = float(input(PROMPT_DAR))
-            work_days_per_week = int(input(PROMPT_WORK_DAYS))
+            dar = float(self.get_input(PROMPT_DAR))
+            work_days_per_week = int(self.get_input(PROMPT_WORK_DAYS))
         except ValueError as e:
-            self.stdout.write(self.style.ERROR(f"Erreur d'entrée: {e}"))
+            self.write_output(f"Erreur d'entrée: {e}", self.style.ERROR)
             return
 
         monthly_income = calculate_monthly_income(dar, work_days_per_week)
-        self.stdout.write(
-            self.style.SUCCESS(Messages.MONTHLY_INCOME.format(monthly_income))
+        self.write_output(
+            Messages.MONTHLY_INCOME.format(monthly_income), self.style.SUCCESS
         )
 
-        modify_days = input(PROMPT_MODIFY_DAYS).lower()
+        modify_days = self.get_input(PROMPT_MODIFY_DAYS).lower()
         while modify_days not in [choice.value for choice in UserChoice]:
-            self.stdout.write(Messages.INVALID_CHOICE)
-            modify_days = input(PROMPT_MODIFY_DAYS).lower()
+            self.write_output(Messages.INVALID_CHOICE, self.style.ERROR)
+            modify_days = self.get_input(PROMPT_MODIFY_DAYS).lower()
 
         if modify_days == UserChoice.YES.value:
             try:
-                new_work_days_per_week = int(input(PROMPT_NEW_WORK_DAYS))
+                new_work_days_per_week = int(self.get_input(PROMPT_NEW_WORK_DAYS))
                 new_dar = adjust_dar(monthly_income, new_work_days_per_week)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        Messages.NEW_DAR.format(new_work_days_per_week, new_dar)
-                    )
+                self.write_output(
+                    Messages.NEW_DAR.format(new_work_days_per_week, new_dar),
+                    self.style.SUCCESS,
                 )
             except ValueError as e:
-                self.stdout.write(self.style.ERROR(f"Erreur d'entrée: {e}"))
+                self.write_output(f"Erreur d'entrée: {e}", self.style.ERROR)
         else:
-            self.stdout.write(Messages.NO_MODIFICATION)
+            self.write_output(Messages.NO_MODIFICATION)
+
+    def write_output(self, message, style_function=None):
+        if self.no_style or style_function is None:
+            self.stdout.write(message)
+        else:
+            self.stdout.write(style_function(message))
+
+    def get_input(self, prompt):
+        return input(prompt)
--- calculator/tests/TF/test_commands.py
+++ calculator/tests/TF/test_commands.py
@@ -5,44 +5,35 @@ from calculator.config import Messages
 
 
 class TestFreelanceCalculateCommand(TestCase):
-    ANSI_SUCCESS_PREFIX = "\x1b[32;1m"
-    ANSI_RESET_SUFFIX = "\x1b[0m"
-
-    @patch("builtins.input", side_effect=["500", "5", "oui", "4"])
+    @patch(
+        "calculator.management.commands.freelance_calculate.Command.get_input",
+        side_effect=["500", "5", "oui", "4"],
+    )
     def test_main_yes_option(self, mock_input):
         command = Command()
         command.stdout = MagicMock()
-        command.handle()
+        command.handle(no_style=True)
 
         expected_calls = [
             call(Messages.REM_TITLE),
-            call(
-                self.ANSI_SUCCESS_PREFIX
-                + Messages.MONTHLY_INCOME.format("10000.0")
-                + self.ANSI_RESET_SUFFIX
-            ),
-            call(
-                self.ANSI_SUCCESS_PREFIX
-                + Messages.NEW_DAR.format(4, "625.0")
-                + self.ANSI_RESET_SUFFIX
-            ),
+            call(Messages.MONTHLY_INCOME.format("10000.0")),
+            call(Messages.NEW_DAR.format(4, "625.0")),
         ]
 
         command.stdout.write.assert_has_calls(expected_calls, any_order=False)
 
-    @patch("builtins.input", side_effect=["500", "5", "non"])
+    @patch(
+        "calculator.management.commands.freelance_calculate.Command.get_input",
+        side_effect=["500", "5", "non"],
+    )
     def test_main_no_option(self, mock_input):
         command = Command()
         command.stdout = MagicMock()
-        command.handle()
+        command.handle(no_style=True)
 
         expected_calls = [
             call(Messages.REM_TITLE),
-            call(
-                self.ANSI_SUCCESS_PREFIX
-                + Messages.MONTHLY_INCOME.format("10000.0")
-                + self.ANSI_RESET_SUFFIX
-            ),
+            call(Messages.MONTHLY_INCOME.format("10000.0")),
             call(Messages.NO_MODIFICATION),
         ]
 
